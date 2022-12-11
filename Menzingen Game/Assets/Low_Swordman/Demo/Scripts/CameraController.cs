5b2de23b9f2391cf0fec15094aa590278bcfa717
Simon7511 <simon.erzinger@gmail.com>
2022-12-11 20:27:39 +0100

    Camera Controll

--- Menzingen Game/Assets/Low_Swordman/Demo/Scripts/CameraController.cs
+++ Menzingen Game/Assets/Low_Swordman/Demo/Scripts/CameraController.cs
@@ -4,32 +4,16 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-
-    public static CameraController Instance;
-
-    public GameObject Target;
-    public int Smoothvalue =2;
-    public float PosY = 1;
-
-
-    // Use this for initialization
-    public Coroutine my_co;
-
+    public Transform target;
+    // Start is called before the first frame update
     void Start()
     {
-     
+
     }
 
-
+    // Update is called once per frame
     void Update()
     {
-        Vector3 Targetpos = new Vector3(Target.transform.position.x, Target.transform.position.y + PosY, -100);
-        transform.position = Vector3.Lerp(transform.position, Targetpos, Time.deltaTime * Smoothvalue);
-
-
-
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
     }
-
-
-
 }
